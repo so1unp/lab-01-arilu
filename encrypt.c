16e27aa083dd687c95ac609e9c11a4d86e23a178
LuciaR22 <luciajazminrivero@gmail.com>
2025-03-27 21:52:42 -0300

    Correcciones encriptador

--- encrypt.c
+++ encrypt.c
@@ -2,63 +2,53 @@
 #include <stdlib.h>
 #include <time.h>
 #include <fcntl.h>
-#include <unistd.h> // Para usar write()
+#include <unistd.h> // Para usar write() y read()
+#include <string.h>
 
 #define MAX_LONGITUD 100
 #define NUM_BYTES_ALEATORIOS 7
+
+// Función genérica para encriptar desde cualquier entrada
+void encriptar(int fd, const char *mensaje, size_t longitud) {
+    for (size_t i = 0; i < longitud; i++) {
+        // Generar y escribir bytes aleatorios
+        for (int j = 0; j < NUM_BYTES_ALEATORIOS; j++) {
+            char charAleatorio = (char)((rand() % 95) + 32); // ASCII imprimible
+            write(fd, &charAleatorio, 1); // Escribir un byte aleatorio
+        }
+        // Escribir el carácter original
+        write(fd, &mensaje[i], 1); // Escribir el carácter actual
+    }
+}
 
 int main(int argc, char *argv[]) {
     // Inicializar semilla para números aleatorios
     srand((unsigned int)time(NULL));
 
     switch (argc) {
-        case 1: { // Leer desde la entrada estándar
-            int caracter = getchar();
-            char palabra[MAX_LONGITUD];
-            while (caracter != EOF) {
-                int indice = 0; 
-                while (caracter != ' ' && caracter != '\n' && caracter != EOF) {
-                    if (indice >= MAX_LONGITUD - 1) { // Validación de longitud
-                        char mensajeError[] = "Error: palabra demasiado larga.\n";
-                        write(STDERR_FILENO, mensajeError, sizeof(mensajeError) - 1); // Escribir en stderr
-                        exit(EXIT_FAILURE);
-                    }
-                    palabra[indice] = (char)caracter;
-                    indice++;
-                    caracter = getchar();
-                }
-                palabra[indice] = '\0';
+        case 1: { // Leer desde la entrada estándar usando read
+            char buffer[MAX_LONGITUD];
+            ssize_t bytesLeidos;
 
-                // Imprimir bytes aleatorios y la palabra
-                for (int i = 0; i < indice; i++) {
-                    for (int j = 0; j < NUM_BYTES_ALEATORIOS; j++) {
-                        char charAleatorio = (char)((rand() % 95) + 32);
-                        write(STDOUT_FILENO, &charAleatorio, 1); // Escribir el byte aleatorio
-                    }
-                    write(STDOUT_FILENO, &palabra[i], 1); // Escribir el carácter original de la palabra
-                }
-                char saltoDeLinea = '\n';
-                write(STDOUT_FILENO, &saltoDeLinea, 1); // Escribir un salto de línea
+            bytesLeidos = read(STDIN_FILENO, buffer, MAX_LONGITUD);
+            if (bytesLeidos == -1) {
+                perror("Error al leer la entrada estándar");
+                exit(EXIT_FAILURE);
             }
+
+            encriptar(STDOUT_FILENO, buffer, bytesLeidos);
+            char saltoDeLinea = '\n'; 
+            write(STDOUT_FILENO, &saltoDeLinea, 1);
+
             break;
         }
         case 2: { // Leer mensaje desde argumentos
             char *mensaje = argv[1];
-            for (int i = 0; mensaje[i] != '\0'; i++) {
-                // Generar y escribir bytes aleatorios
-                for (int k = 0; k < NUM_BYTES_ALEATORIOS; k++) {
-                    char charAleatorio = (char)((rand() % 95) + 32); // ASCII imprimible
-                    write(STDOUT_FILENO, &charAleatorio, 1); // Escribir un byte aleatorio
-                }
-                // Escribir el carácter original del mensaje
-                write(STDOUT_FILENO, &mensaje[i], 1); // Escribir el carácter actual
-            }
-            // Escribir un salto de línea al final
-            char saltoDeLinea = '\n';
-            write(STDOUT_FILENO, &saltoDeLinea, 1); // Escribir un salto de línea
+            encriptar(STDOUT_FILENO, mensaje, strlen(mensaje));
+            char saltoDeLinea = '\n'; 
+            write(STDOUT_FILENO, &saltoDeLinea, 1);
             break;
         }
-        
         case 3: { // Leer mensaje y guardar en archivo
             int archivoSalida = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
             if (archivoSalida == -1) {
@@ -67,27 +57,20 @@ int main(int argc, char *argv[]) {
             }
 
             char *mensajeEntrada = argv[2];
-            for (int i = 0; mensajeEntrada[i] != '\0'; i++) {
-                for (int j = 0; j < NUM_BYTES_ALEATORIOS; j++) {
-                    char charAleatorio = (char)((rand() % 95) + 32);
-                    write(archivoSalida, &charAleatorio, 1);
-                }
-                write(archivoSalida, &mensajeEntrada[i], 1);
-            }
-            char saltoDeLinea = '\n';
+            
+            encriptar(archivoSalida, mensajeEntrada, strlen(mensajeEntrada));
+            char saltoDeLinea = '\n'; 
             write(archivoSalida, &saltoDeLinea, 1);
 
-            close(archivoSalida);
+            close(archivoSalida); // Cerrar el archivo
             break;
         }
         default: {
-            char mensjError[] = "Uso incorrecto. Intenta con 1 o 2 argumentos.\n";
-            write(STDIN_FILENO, mensjError, sizeof(mensjError) - 1);
-            
+            char mensajeError[] = "Uso incorrecto. Intenta con 2 o 3 argumentos.\n";
+            write(STDERR_FILENO, mensajeError, sizeof(mensajeError) - 1);
             break;
         }
     }
 
-    exit(EXIT_SUCCESS);
+    return 0;
 }
-
